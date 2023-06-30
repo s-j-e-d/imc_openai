563457264d089f0df8d717607562471c16730037
s-j-e-d <95914269+s-j-e-d@users.noreply.github.com>
2023-06-30 15:55:09 +0200

    Commit5

--- fetch-kobo-data.js
+++ fetch-kobo-data.js
@@ -1,16 +1,24 @@
 // fetch-kobo-data.js
 const fs = require('fs');
 const axios = require('axios');
+const XLSX = require('xlsx');
 
 const token = process.env.KOBO_TOKEN;  // Environment variable for your token
 
-axios.get('https://kobo.humanitarianresponse.info/api/v2/assets/aCuJ5p5ekgWJXRZGNs3j5Q/export-settings/escHgbQm4kqHUF5cjcX2CN2/data.xlsx', {
+axios({
+    url: 'https://kobo.humanitarianresponse.info/api/v2/assets/aCuJ5p5ekgWJXRZGNs3j5Q/export-settings/escHgbQm4kqHUF5cjcX2CN2/data.xlsx',
+    method: 'GET',
+    responseType: 'arraybuffer', // Important for binary files
     headers: {
-      'Authorization': `Token ${token}`
-    }
+      'Authorization': `Token ${token}`,
+      'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    },
   })
   .then(response => {
-    fs.writeFileSync('data.json', JSON.stringify(response.data));
+    const workbook = XLSX.read(response.data, {type: 'buffer'});
+    const sheetNameList = workbook.SheetNames;
+    const json = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]]);
+    fs.writeFileSync('data.json', JSON.stringify(json));
   })
   .catch(error => {
     console.error(error);
