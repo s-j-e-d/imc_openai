12efe1fa625b565fc88ef8b01aa8bffab3bdd462
s-j-e-d <95914269+s-j-e-d@users.noreply.github.com>
2023-06-30 15:45:06 +0200

    Commit4

--- fetch-kobo-data.js
+++ fetch-kobo-data.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const token = process.env.KOBO_TOKEN;  // Environment variable for your token
 
-axios.get('https://your-kobo-api-endpoint', {
+axios.get('https://kobo.humanitarianresponse.info/api/v2/assets/aCuJ5p5ekgWJXRZGNs3j5Q/export-settings/escHgbQm4kqHUF5cjcX2CN2/data.xlsx', {
     headers: {
       'Authorization': `Token ${token}`
     }
